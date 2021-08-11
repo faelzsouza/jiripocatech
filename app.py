4398ea480c9f5197915739b2b1b36a86f114fafa
Flavio Wu <flaviowu@gmail.com>
2021-08-10 21:24:56 -0300

    func

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from config import chave_secreta, url_postgresql
 import yt
@@ -41,6 +41,28 @@ def courses():
 def about():
     return render_template('about.html')
 
+# Rota Login
+@app.route('/login')
+def login():
+    session['usuario_logado'] = None # Desloga o usuario
+    return redirect('/courses')
+
+# rota autenticação
+@app.route('/auth', methods=['GET', 'POST']) # Rota de autenticação
+def auth():
+    if request.form['senha'] == 'admin': # Se a senha for 'admin' faça:
+      session['usuario_logado'] = 'admin' # Adiciona um usuario na sessão
+      flash('Login feito com sucesso!') # Envia mensagem de sucesso
+      return redirect('/adm') # Redireciona para a rota adm
+    else: # Se a senha estiver errada, faça:
+      flash('Erro no login, tente novamente!')  # Envia mensagem de erro
+      return redirect('/login') # Redireciona para a rota login
+
+@app.route('/logout') # Rota para deslogar
+def logout():
+   session['usuario_logado'] = None # Deixa o usuario_logado vazio
+   return redirect('/login') # Redireciona para a rota principal (index.html)
+
 # Adm - Adicionar - Início
 @app.route('/adicionar')
 def adicionar():
@@ -51,21 +73,23 @@ def adicionar():
 def id():
     playlists = Playlist.query.all()
     playlist = playlists.query.get(id)
-    return render_template('adm.html', playlist = playlist)    
+    return render_template('adm.html', playlist = playlist, titulo='Editar')    
 
 # ROTA EDIT
 @app.route('/edit/<id>', methods=['GET', 'POST'])
-def edit():
+def edit(id):
+    # id = id[1:-1]
     playlists = Playlist.query.all()
     playlist = Playlist.query.get(id)
     if request.method == 'POST':
-        playlist.id_playlist = request.form['id-playlist']
+        playlist.id_playlist = request.form['id-playlist'].split('=')[1]
+        playlist.instrutor = request.form['instrutor']
         playlist.titulo_playlist = request.form['titulo-playlist']
-        playlist.descricao = request.form['descricao']
+        playlist.descricao = request.form['descricao-playlist']
         db.session.commit()
         db.session.close()
-        return redirect('/adm')
-    return render_template('adm.html', playlist = playlist) 
+        return redirect('/courses')
+    return render_template('adm.html', playlist = playlist, titulo='Editar') 
 
 # rota NEW para adicionar ao banco
 @app.route('/new', methods=['GET', 'POST'])
